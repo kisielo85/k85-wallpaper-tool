70298531d31a28d82a3115d43a8cb4d0b6af7815
Przemek B <pprzemek89@gmail.com>
2025-11-11 00:34:11 +0100

    linux release bugfix

--- build_linux.sh
+++ build_linux.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 read -p "version: v" v
-pyinstaller --onefile --name="k85.wallpaper.tool.linux.v${v}" --add-data "assets/*_img.png:assets" --add-data "assets/icon.png:assets" --noconfirm --noconsole main.py
+pyinstaller --onefile --name="k85.wallpaper.tool.linux.v${v}" --add-data "assets/*_img.png:assets" --add-data "assets/icon.png:assets" --hidden-import PIL._tkinter_finder --noconfirm --noconsole main.py
